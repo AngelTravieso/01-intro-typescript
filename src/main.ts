b7e861c8511f85a2d363f1bdaa892a8c8934a89b
AngelTravieso <angeltraviesoc@gmail.com>
2023-06-29 10:43:59 -0400

    Decorador de método - Deprecated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { setupCounter } from './counter.ts'
 // import { pokemons } from './bases/02-objects.ts'
 // import { charmander } from './bases/03-classes.ts'
 // import { charmander } from './bases/04-injection.ts'
-import { charmander } from './bases/05-decorators.ts'
+// import { charmander } from './bases/05-decorators.ts'
+import { charmander } from './bases/06-decorators2.ts'
 
 
 
